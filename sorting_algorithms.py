6c72101355735bb3adcac0fef8a7c99a8c89424b
nikolaithefirst <87936349+nikolaithefirst@users.noreply.github.com>
2023-10-29 18:21:24 +0300

    add binary search algorithm

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -1,8 +1,11 @@
+import numpy as np
+
+
 def linear_sort(A:list, n:int):
     """
     Функция линейного поиска позиции элемента n в массиве A
     :param A: массив элементов размера k
-    :param n: искомый элемент
+    :param n: искомое значение
     :return: A(k) = n, если k есть в A или "Not found", если n не в A
     """
 
@@ -15,10 +18,10 @@ def linear_sort(A:list, n:int):
 def sentinel_linear_search(A:list, n:int, x:int):
     """
     Функция линейного поиска позиции элемента n в массиве A
-    :param A: массив элементов размера k
+    :param A: массив элементов размера n
     :param n: размер массива
-    :param x: искомый элемент
-    :return: A(k) = n, если k есть в A или "Not found", если n не в A
+    :param x: искомое значение
+    :return: позиция i, если A(i) = x, иначе "Not found"
     """
     last = A[n-1]
     A[n-1] = x
@@ -28,3 +31,27 @@ def sentinel_linear_search(A:list, n:int, x:int):
     if i < (n - 1) or last == n-1:
         return i
     return "Not found"
+
+
+def binary_search(A:list, n:int, x:int):
+    """
+    Функция бинарного поиска позиции x в отсортированном массиве A
+    :param A: отсортированный массив элементов размера n
+    :param n: размер массива
+    :param x: искомое значение
+    :return: позиция i, если A(i) = x, иначе "Not found"
+    """
+    p, r = 0, n - 1
+    while p <= r:
+        q = (p + r) // 2
+        if A[q] == x:
+            return q
+        elif A[q] > x:
+            r = q - 1
+        elif A[q] <= x:
+            p = q + 1
+    return "Not found"
+
+
+if __name__ == "__main__":
+    print(binary_search.__doc__)
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
 import sorting_algorithms
+import numpy as np
 
 
 class MyTestCase(unittest.TestCase):
@@ -18,6 +19,22 @@ class MyTestCase(unittest.TestCase):
     def test_case_5(self):
         self.assertEqual(sorting_algorithms.sentinel_linear_search([3, 25, 6, 576, 4, 8, 56, 5984, 0, 11], 10, 22),
                          "Not found")
+
+    def test_case_6(self):
+        self.assertEqual(sorting_algorithms.sentinel_linear_search([3, 22, 6, 25, 4, 8, 56, 25, 0, 11], 10, 25),
+                         3)
+
+    def test_case_7(self):
+        self.assertEqual(sorting_algorithms.binary_search(np.arange(0, 1000, 1, dtype=int), 1001, 125), 125)
+
+    def test_case_8(self):
+        self.assertEqual(sorting_algorithms.binary_search(np.arange(0, 1000, 1, dtype=int), 1001, 825), 825)
+
+    def test_case_9(self):
+        self.assertEqual(sorting_algorithms.binary_search(np.arange(0, 1001, 1, dtype=int), 1001, 1002), "Not found")
+
+    def test_case_10(self):
+        self.assertEqual(sorting_algorithms.binary_search(np.arange(1, 1001, 1, dtype=int), 1000, 0), "Not found")
 
 
 if __name__ == '__main__':
