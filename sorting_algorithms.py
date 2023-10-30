09e6c6c3d28ee60f6cf64c4d75e139014bebd46f
nikolaithefirst <87936349+nikolaithefirst@users.noreply.github.com>
2023-10-30 18:24:34 +0300

    Add files via upload
    Add insert_sort algorithm

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import time
 
 def linear_sort(A:list, n:int):
     """
@@ -64,14 +64,66 @@ def selection_sort(A:list, n:int):
         smallest = i
         for j in range(i+1, n):
             if A[j] < A[smallest]:
-                # smallest = i
-                temp = A[i]
-                A[i] = A[j]
-                A[j] = temp
+                smallest = j
+        temp = A[i]
+        A[i] = A[smallest]
+        A[smallest] = temp
     return A
+
+
+def selection_sort_2(A:list, n:int):
+    """
+    Функция сортировки выбором
+    :param A: массив элементов размера n
+    :param n: размер массива
+    :return: отсортированный по возрастанию массив A
+    """
+    for i in range(n-1):
+        smallest = i
+        for j in range(i+1, n):
+            if A[j] < A[smallest]:
+                smallest = j
+                temp = A[i]
+                A[i] = A[smallest]
+                A[smallest] = temp
+    return A
+
+
+def inserion_sort(A, n):
+    """
+    Функция сортировки вставкой
+    :param A: массив элементов размера n
+    :param n: размер массива
+    :return: отсортированный по возрастанию массив A
+    """
+    for i in range(1, n):
+        key = A[i]
+        j = i - 1
+        while j > -1 and A[j] > key:
+            A[j+1] = A[j]
+            j -= 1
+        A[j+1] = key
+    return A
+
 
 
 if __name__ == "__main__":
     print(binary_search.__doc__)
-    a = np.random.randint(1, 100, 100)
-    print(selection_sort(a, len(a)))
+    a = np.random.randint(1, 1000, 1000)
+    start = time.time()
+    selection_sort(a, len(a))
+    print(time.time() - start)
+    b = np.random.randint(1, 1000, 1000)
+    start = time.time()
+    selection_sort_2(b, len(b))
+    print(time.time() - start)
+    c = np.arange(0, 100000)
+    start = time.time()
+    find = sentinel_linear_search(c, len(c), 999)
+    print(time.time() - start, find)
+    start = time.time()
+    find = binary_search(c, len(c), 999)
+    print(time.time() - start, find)
+    d = np.random.randint(1, 100, 100)
+    print(d)
+    print(inserion_sort(d, len(d)))
